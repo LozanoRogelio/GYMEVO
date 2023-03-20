7634f6dcf6d8a9f2b371251ecc55697cfd121f61
Rogelio Lozano <lozano.rogelio92@gmail.com>
2023-03-20 15:09:22 -0500

    Using typescript and modifying lowecase text in Navbar

--- src/scenes/navbar/Link.tsx
+++ src/scenes/navbar/Link.tsx
@@ -1,13 +1,25 @@
-import AnchorLink from "react-anchor-link-smooth-scroll"
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
-type Props = {}
+type Props = {
+  page: string;
+  selectedPage: SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void;
+};
 
-const Link = (props: Props) => {
-   return (
-      <AnchorLink>
-         {page}
-      </AnchorLink>
-   )
-}
+const Link = ({ page, selectedPage, setSelectedPage }: Props) => {
+  const lowerCasePage = page.toLowerCase().replace(/ /g, "") as SelectedPage;
 
-export default Link
+  return (
+    <AnchorLink
+      className={`${selectedPage === lowerCasePage ? "text-primary-500" : ""}
+        transition duration-500 hover:text-primary-300
+      `}
+      href={`#${lowerCasePage}`}
+      onClick={() => setSelectedPage(lowerCasePage)}
+    >
+      {page}
+    </AnchorLink>
+  );
+};
+
+export default Link;
