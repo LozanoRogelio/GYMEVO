a0d87857ebd1447a42b71d2c3ff4942403641865
Rogelio Lozano <lozano.rogelio92@gmail.com>
2023-03-23 11:18:59 -0500

    Fixing the scroll method on navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import Navbar from "@/scenes/navbar";
 import { useState } from "react";
 import { SelectedPage } from "@/shared/types";
+import { useEffect } from "react";
 
 
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY == 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    }
+  })
 
   return (
     <div className="app bg-gray-20">
