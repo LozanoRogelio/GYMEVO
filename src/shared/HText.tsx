0737e72bef0eb7bad65675582af839af1db6b940
Rogelio Lozano <lozano.rogelio92@gmail.com>
2023-03-24 23:07:16 -0500

    Header text update

---
+++ src/shared/HText.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+type Props = {}
+
+const HText = (props: Props) => {
+   return (
+      <h1 className="basis-3/5 font-montserrat text-3xl font-bold"
+      ></h1>
+   )
+};
+
+export default HText
